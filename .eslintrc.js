16aed8919762d98b19c46005b58abfe467410752
Brennon Schow <me@brennongs.dev>
2025-04-18 09:35:30 -0600

    feat: initial functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'node', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'react'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
